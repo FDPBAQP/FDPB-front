f88d9d51bafb2cb2290300ace880ce3dff309bf2
Renzo Davila <renzo.davila@tecsup.edu.pe>
2022-04-15 17:53:22 -0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  server: "http://localhost:4000/" //dev
+  // server: "http://localhost:4000/" //dev
+  server: "http://100.24.59.157:4000/" //prod
 };
